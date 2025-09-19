15145ad4da3e88cce55cf50e30262541320ac98f
arkoroy05 <arkoroy302@gmail.com>
2025-09-19 18:38:43 +0530

    auto pop fix

--- front/app/providers.tsx
+++ front/app/providers.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "@/components/ui/sonner"
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient()
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config={config} reconnectOnMount={false}>
       <QueryClientProvider client={queryClient}>
         {children}
         <Toaster position="top-right" richColors />
